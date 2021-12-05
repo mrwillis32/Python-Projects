47ba02f43c0a5bef34fb1f4f8a1b496216b735a5
mrwillis32 <83623388+mrwillis32@users.noreply.github.com>
2021-12-05 12:52:50 -0500

    revison fileList

--- fileList.py
+++ fileList.py
@@ -10,15 +10,8 @@ conn = sqlite3.connect('tables2.db')
 with conn:
     cur = conn.cursor()
     # the syntax below will create our database and add the columns that we will need
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_fileLists( \
-        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_docx TEXT, \
-        col_txt TEXT, \
-        col_png TEXT, \
-        col_mpg TEXT, \
-        col_pdf TEXT, \
-        col_jpg TEXT \
-        )")
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_fileList(ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                  col_txt TEXT)")
     conn.commit()
     # conn.commit() commits the changes we just made to our database 
 conn.close()
@@ -26,34 +19,16 @@ conn.close()
 
 conn = sqlite3.connect('tables2.db')
 
+fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
 
-with conn:# with conn let's us know that as long as we are connected to our database run the below code.
-    # here I iterated through the function to select our two file ext with '.txt'
-     fileList = ('information.docx', 'Hello.txt', 'myImage.png', \ 'myMovie.mpg', 'World.txt','data.pdf','myPhoto.jpg')
-for extensions in fileList:
-  print('Hello.txt','World.txt')
-        conn.commit()
-conn.close()
-# here we will be adding our values into our table
-
-
-conn = sqlite3.connect('tables2.db')
-
-
-with conn:  
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_fileLists(col_txt) VALUES (?)"
-                ( 'Hello.txt'))
-    conn.commit()
-conn.close()
-
-
-conn = sqlite3.connect('tables2.db')
-
-
-with conn:  
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_fileLists(col_txt) VALUES (?)"
-                ('World.txt'))
-    conn.commit()
+# loop through each object in the tuple to find the names that end in .txt.
+for x in fileList:
+  if x.endswith('.txt'):
+    with conn:
+      cur = conn.cursor()
+    # the value for each row will be one name out of the tuple therefore (x,)
+    # will denote a one element tuple for each name ening with .txt.
+      cur.execute("INSERT INTO tbl_fileList (col_txt) VALUES (?)", (x,))
+      print(x)
 conn.close()
