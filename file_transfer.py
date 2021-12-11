1860605829e461d9adb616ac9b3c5d04ee8c2913
mrwillis32 <83623388+mrwillis32@users.noreply.github.com>
2021-12-11 01:22:03 -0500

    Update file_transfer.py

--- file_transfer.py
+++ file_transfer.py
@@ -1,98 +1,218 @@
 
 
 
-
-
+# Importing necessary packages
 import shutil
-import os
-
-
-
-
-#set where the source of the files are
-source = '/Users/mr.willis/Desktop/Folder A/'
-
-#set the destination path to folder b
-destination = '/Users/mr.willis/Desktop/Folder B/'
-files = os.listdir(source)
-
-for i in files:
-    #We are saying move the files represented by 'i' to their new destination
-    shutil.move(source+i, destination)
-
-import shutil
-import os
-
-
-#set where the source of the files are
-source = '/Users/mr.willis/Desktop/Daily Files/'
-
-destination = '/Users/mr.willis/Desktop/Home Office Files/'
-files = os.listdir(source)
-
-for i in files:
-    shutil.move(source+i, destination)
-
-
-#Python program to create
-# a file explorer in Tkinter
-
-# import all componets
-# from the tkinter library
+import tkinter as tk
 from tkinter import *
+from tkinter import messagebox, filedialog
+	
 
-# import filedialog module
-from tkinter import filedialog
+# Defining CreateWidgets() function to
+# create necessary tkinter widgets
+def CreateWidgets():
+	link_Label = Label(root, text ="Select The File To Copy : ",
+					bg = "#E8D579")
+	link_Label.grid(row = 1, column = 0,
+					pady = 5, padx = 5)
+	
+	root.sourceText = Entry(root, width = 50,
+							textvariable = sourceLocation)
+	root.sourceText.grid(row = 1, column = 1,
+						pady = 5, padx = 5,
+						columnspan = 2)
+	
+	source_browseButton = Button(root, text ="Browse",
+								command = SourceBrowse, width = 15)
+	source_browseButton.grid(row = 1, column = 3,
+							pady = 5, padx = 5)
+	
+	destinationLabel = Label(root, text ="Select The Destination : ",
+							bg ="#E8D579")
+	destinationLabel.grid(row = 2, column = 0,
+						pady = 5, padx = 5)
+	
+	root.destinationText = Entry(root, width = 50,
+								textvariable = destinationLocation)
+	root.destinationText.grid(row = 2, column = 1,
+							pady = 5, padx = 5,
+							columnspan = 2)
+	
+	dest_browseButton = Button(root, text ="Browse",
+							command = DestinationBrowse, width = 15)
+	dest_browseButton.grid(row = 2, column = 3,
+						pady = 5, padx = 5)
+	
+	copyButton = Button(root, text ="Copy File",
+						command = CopyFile, width = 15)
+	copyButton.grid(row = 3, column = 1,
+					pady = 5, padx = 5)
+	
+	moveButton = Button(root, text ="Move File",
+						command = MoveFile, width = 15)
+	moveButton.grid(row = 3, column = 2,
+					pady = 5, padx = 5)
 
-# Function for opening the
-# file explorer window
-def browseFiles():
-    filename = filedialog.askopenfilename(initialdir = "/",
-                                          title = "Select a File",
-                                          filetypes = (("Text files",
-                                                        "*.txt*"),
-                                                       ("all files",
-                                                        "**.*")))
+def SourceBrowse():
+	
+	# Opening the file-dialog directory prompting
+	# the user to select files to copy using
+	# filedialog.askopenfilenames() method. Setting
+	# initialdir argument is optional Since multiple
+	# files may be selected, converting the selection
+	# to list using list()
+	root.files_list = list(filedialog.askopenfilenames(initialdir ="/Users/mr.willis / Desktop /Daily Files"))
+	
+	# Displaying the selected files in the root.sourceText
+	# Entry using root.sourceText.insert()
+	root.sourceText.insert('1', root.files_list)
+	
+def DestinationBrowse():
+	# Opening the file-dialog directory prompting
+	# the user to select destination folder to
+	# which files are to be copied using the
+	# filedialog.askopendirectory() method.
+	# Setting initialdir argument is optional
+	destinationdirectory = filedialog.askdirectory(initialdir ="/Users/mr.willis/Desktop/Daily Files")
 
-    # Create label contents
-    label_file_explorer.configure(text="File Opened: "+filename)
+	# Displaying the selected directory in the
+	# root.destinationText Entry using
+	# root.destinationText.insert()
+	root.destinationText.insert('1', destinationdirectory)
+	
+def CopyFile():
+	# Retrieving the source file selected by the
+	# user in the SourceBrowse() and storing it in a
+	# variable named files_list
+	files_list = root.files_list
+
+	# Retrieving the destination location from the
+	# textvariable using destinationLocation.get() and
+	# storing in destination_location
+	destination_location = destinationLocation.get()
+
+	# Looping through the files present in the list
+	for f in files_list:
+		
+		# Copying the file to the destination using
+		# the copy() of shutil module copy take the
+		# source file and the destination folder as
+		# the arguments
+		shutil.copy(f, destination_location)
+
+	messagebox.showinfo("SUCCESSFUL")
+	
+def MoveFile():
+	
+	# Retrieving the source file selected by the
+	# user in the SourceBrowse() and storing it in a
+	# variable named files_list'''
+	files_list = root.files_list
+
+	# Retrieving the destination location from the
+	# textvariable using destinationLocation.get() and
+	# storing in destination_location
+	destination_location = destinationLocation.get()
+
+	# Looping through the files present in the list
+	for f in files_list:
+		
+		# Moving the file to the destination using
+		# the move() of shutil module copy take the
+		# source file and the destination folder as
+		# the arguments
+		shutil.move(f, destination_location)
+
+	messagebox.showinfo("SUCCESSFUL")
+
+# Creating object of tk class
+root = tk.Tk()
+	
+# Setting the title and background color
+# disabling the resizing property
+root.geometry("830x120")
+root.title("Copy-Move GUI")
+root.config(background = "black")
+	
+# Creating tkinter variable
+sourceLocation = StringVar()
+destinationLocation = StringVar()
+	
+# Calling the CreateWidgets() function
+CreateWidgets()
+	
+# Defining infinite loop
+root.mainloop()
 
 
+# Import the following modules
+import os
+import time
+import shutil
+import datetime
+import glob
 
-# Create the root window
-window = Tk()
 
+# Change the directory and jump to the location
+# where you want to arrange the files
+os.chdir(r"/Users/mr.willis/Desktop/Home Office Files")
 
-# Set window title
-window.title('File Explorer')
+# List the directories and make a list
+all_files = list(os.listdir())
 
-# Set window size
-window.geometry("500x500")
+# Get the current working directory
+outputs = os.getcwd()
 
-#Set window background color
-window.config(background = "white")
+# Run a loop for traversing through all the
+# files in the current directory
+for files in all_files:
+	try:
+		
+		# Jump to the directories files
+		inputs = glob.glob(files+"\\*")
+		
+		# Now again run a loop for travering through
+		# all the files inside the folder
+		for ele in inputs:
+			
+			# Now, move the files one-by-one
+			shutil.move(ele, outputs)
+		
+		# After extracting files from the folders,
+		# delete that folder
+		shutil.rmtree(files)
+	except:
+		pass
 
-# Create a File Explorer label
-label_file_explorer = Label(window,
-                            text = "File Explorer using Tkinter",
-                            width = 100, height = 4,
-                            fg = "blue")
+# Again run a loop for traversing through all the
+# files in the current directory
+for files in os.listdir('/Users/mr.willis/Desktop/Daily Files'):
+	
+	# Get all the details of the file creation
+	# and modification
+	time_format = time.gmtime(os.path.getmtime(files))
+	
+	# Now, extract only the Year, Month, and Day
+	datetime_object = datetime.datetime.strptime(str(time_format.tm_mon), "%m")
+	
+	# Provide the number and find the month
+	full_month_name = datetime_object.strftime(
+		"%b")
+	
+	# Give the name of the folder
+	dir_name = full_month_name + 'Daily Files' + \
+		str(time_format.tm_mday) + "Daily Files" + \
+		str(time_format.tm_year)
 
-button_explore = Button(window,
-                        text = "Browse Files",
-                        command = browseFiles)
+	# Check if the folder exists or not
+	if not os.path.isdir(dir_name):
+		
+		# If not then make the new folder
+		os.mkdir(dir_name)
+	dest = dir_name
+	
+	# Move all the files to their respective folders
+	shutil.move(files, dest)
+	
+print("successfully moved...")
 
-button_exit = Button(window,
-                     text = "Exit",
-                     command = exit)
-
-# Grid method is chosen for placing
-# the widgets at respective positions
-# in a table like structure by
-# specifying rows and columns
-label_file_explorer.grid(column = 1, row = 1)
-
-button_explore.grid(column =1, row = 2)
-
-# Let the window wait for any events
-window.mainloop()
