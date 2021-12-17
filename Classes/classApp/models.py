8f6aa1b4cb06e79e588b414179b948244f60f077
mrwillis32 <83623388+mrwillis32@users.noreply.github.com>
2021-12-16 23:46:02 -0500

    django assignment

--- Classes/classApp/models.py
+++ Classes/classApp/models.py
@@ -1,37 +1,23 @@
+
+
 from django.db import models
 
+
 # created our objects
-TITLE_CHOICES = {
-    ('Django 101', 'Django 101'),
-    ('Django 102','Django 102'),
-    ('Django 103','Django 103'),
-}
 
-COURSE_NUMBER = {
-    ('100','100'),
-    ('102','102'),
-    ('200','200'),
-}
-
-INSTRUCTOR_CHOICES = {
-    ('K.Bryant','K.Bryant'),
-    ('A.Jones','A.Jones'),
-    ('M.Hatter','M.Hatter'),
-}
-
-DURATION_CHOICES = {
-    (30,30),
-    (60,60),
-    (90,90),
-}
 # Create your models here.
+
+
 class DjangoClasses(models.Model):
-    title = models.CharField(max_length=30, choices=TITLE_CHOICES)
-    course_number = models.IntegerField(choices=COURSE_NUMBER)
-    instructor = models.CharField(max_length=30, choices=INSTRUCTOR_CHOICES)
-    duration = models.DurationField(choices=DURATION_CHOICES)
+    title = models.CharField(max_length=30)
+    course_number = models.IntegerField(max_length=200)
+    instructor = models.CharField(max_length=30)
+    duration = models.DurationField()
 
-    objects = models.Manager()
 
-    def __str__(self):
-        return self.name
+DjangoClasses.objects.create(title='SE 101', course_number=100, instructor='K.Bryant', duration=30.0)
+objects = models.Manager()
+
+
+def __str__(self):
+    return self.name
